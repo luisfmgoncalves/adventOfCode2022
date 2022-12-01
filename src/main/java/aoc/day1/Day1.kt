0e820cec5583ed28f048e2c2ca1f5df1cf2b1d84
luisfmgoncalves <luisfmgoncalves@gmail.com>
2022-12-02 00:44:35 +0100

    Day 1

---
+++ src/main/java/aoc/day1/Day1.kt
@@ -0,0 +1,18 @@
+package aoc.day1
+
+import aoc.utils.FileUtils
+
+fun main() {
+    val readLines = FileUtils.readInput("day1").readLines()
+
+    val calories = mutableListOf<Int>()
+    readLines.fold(0) { acc, element ->
+        if (element.isEmpty()) {
+            calories.add(acc)
+            0
+        } else acc + element.toInt()}
+    calories.sortDescending()
+
+    println("Part 1: " + calories[0]) //66487
+    println("Part 2: " + calories.take(3).sum()) //197301
+}
---
+++ src/main/java/aoc/utils/FileUtils.kt
@@ -0,0 +1,12 @@
+package aoc.utils
+
+import java.io.File
+
+object FileUtils {
+
+    fun readInput(day : String): File {
+        val dir = System.getProperty("user.dir")
+        return File("$dir/src/main/java/aoc/$day/input.txt")
+    }
+
+}
