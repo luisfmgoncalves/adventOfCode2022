c4659ca508d2ced5d8ddf7712fbb8eee628e1bc6
luisfmgoncalves <luisfmgoncalves@gmail.com>
2022-12-06 22:16:03 +0100

    Day 6

---
+++ src/main/java/aoc/day6/Day6.kt
@@ -0,0 +1,33 @@
+package aoc.day6
+
+import aoc.utils.FileUtils
+import java.nio.charset.StandardCharsets.UTF_8
+
+fun main() {
+    val input = FileUtils.readInput("day6").readText(UTF_8)
+
+    println("Part 1: ${ run(input, 4) }") //1794
+    println("Part 2: ${ run(input, 14) }") //2851
+}
+
+fun run(input: String, segmentSize: Int) : Int {
+    var current = 0
+    while (current + segmentSize <= input.length) {
+        val chars = input.substring(current, current + segmentSize).toMutableList()
+        val knowChars = mutableListOf<Char>()
+        for(c in chars) {
+            if (knowChars.contains(c)) { break }
+            else knowChars.add(c)
+        }
+        if(knowChars.size == segmentSize) { break }
+        current++
+    }
+    return current + segmentSize;
+}
+
+
+
+
+
+
+
